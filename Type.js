cbfe41a9e4c25d9df6b8a0a69833c3dfeaec9f28
Daiamon20 <Daiamon20@info.com>
2020-07-06 11:40:48 +0200

    lol

---
+++ Type.js
@@ -0,0 +1,12 @@
+function type(Text) {
+  setTimeout(function() {
+    let T = toString(Text)
+    T = Text + "i"
+    T = Text.length
+    let te;
+    for (i = 0; i <= T; i++) {
+        te = Text.slice(0,i)
+        document.getElementById("Contentx").innerHTML = ""
+    }
+  }, 400)
+}
