99569ac0eb55006377fde5491464c4b0b31f3f78
Daiamon20 <Daiamon20@info.com>
2020-07-06 19:56:57 +0200

    hm

--- Type.js
+++
@@ -1,9 +0,0 @@
-let Vel = 100;
-
-function type(Text) {
-    var T = toString(Text);
-    for (i = 0 ; i < T.length ; i++) {
-        document.getElementById("Output").innerHTML += Text.charAt(i);
-        setTimeout(type, Vel)
-    }
-}
